1aa9f018dccee69def173d3aaa7becef1dae96e0
Olesia Vysotina <90894720+Olesia2805@users.noreply.github.com>
2024-10-20 17:58:18 +0000

    added NotFoundPade

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,10 @@ const App = () => {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/movie" element={<MoviePage />} />
+          {/* <Route path="/movie/:movieId" element={<MovieDetailsPage />} >
+            <Route path="cast" element={<MovieCast />} />
+            <Route path="reviews" element={<MovieReviews />} />
+          </Route> */}
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </Section>
--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,7 +1,27 @@
-// import notFoundCss from './NotFoundPage.module.css'
+import { Link } from 'react-router-dom';
+import notFoundCss from './NotFoundPage.module.css';
 
 const NotFoundPage = () => {
-  return <div>Home</div>;
+  return (
+    <section className={notFoundCss.page_404}>
+      <div className={notFoundCss.container}>
+        <div className={notFoundCss.row}>
+          <div className={notFoundCss.colSm12}>
+            <div className={`${notFoundCss.colSm10} ${notFoundCss.colSmOffset1} text-center`}>
+              <div className={notFoundCss.fourZeroFourBg}>
+                <h1 className="text-center">404</h1>
+              </div>
+              <div className={notFoundCss.contantBox404}>
+                <h3 className={notFoundCss.h2}>Looks like you're lost</h3>
+                <p>The page you are looking for is not available!</p>
+                <Link to="/" className={notFoundCss.link404}>Go to Home</Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default NotFoundPage;
