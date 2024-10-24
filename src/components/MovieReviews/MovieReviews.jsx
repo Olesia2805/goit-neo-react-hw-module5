2ba62aff5554c2083880775d01b0c8e47c2ca07b
Olesia Vysotina <vysotina2002@gmail.com>
2024-10-24 19:50:25 +0200

    Reviews block is done

--- src/components/MovieReviews/MovieReviews.jsx
+++ src/components/MovieReviews/MovieReviews.jsx
@@ -1,19 +1,46 @@
-import ReviewCard from '../ReviewCard/ReviewCard';
 import reviewsCss from './MovieReviews.module.css';
+import ReviewCard from '../ReviewCard/ReviewCard';
+import Loader from '../Loader/Loader';
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { fetchMovieReviews } from '../../api/movies-api';
 
-const MovieReviews = ({ reviews }) => {
+const MovieReviews = () => {
+  const { movieId } = useParams();
+  const [reviews, setReviews] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const getReviews = async () => {
+      try {
+        setIsLoading(true);
+        setError(null);
+
+        const reviewData = await fetchMovieReviews(movieId);
+        setReviews(reviewData);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    getReviews();
+  }, [movieId]);
+
   return (
     <>
+      {isLoading && <Loader />}
+      {error && <p>Something went wrong. Please try again.</p>}
       <ul className={reviewsCss.list}>
-        {reviews.length > 0 ? (
-          reviews.map(review => (
-            <li key={review.id} className={reviewsCss.cardWrapper}>
-              <ReviewCard review={review} />{' '}
-            </li>
-          ))
-        ) : (
-          <p>No reviews available for this movie.</p>
-        )}
+        {reviews?.length > 0
+          ? reviews.map(review => (
+              <li key={review.id} className={reviewsCss.cardWrapper}>
+                <ReviewCard review={review} />{' '}
+              </li>
+            ))
+          : !isLoading && !error && <p>No reviews available for this movie.</p>}
       </ul>
     </>
   );
